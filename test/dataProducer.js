fa23a8f966b8e2aa0486cdf3f01bcfd66892cd4b
loviselu <lovise.lu@gmail.com>
2013-04-02 14:36:58 +0800

    commit test

--- test/dataProducer.js
+++ test/dataProducer.js
@@ -2,4 +2,26 @@ var dp = require('../dataProducer');
 
 dp.create({title: 'haha'}, function (err, id) {
 	console.log(id);
+	dp.get(id, function (err, data) {
+		console.log(data);
+	})
+	dp.update(id, {"A2": {"before": "34", now: "7"}}, function (err, data) {
+		console.log(data);
+		dp.get(id, function (err, data) {
+			console.log(data);
+		});
+		dp.update(id, {"A2": {"before": "34", now: "7"}}, function (err, data) {
+			console.log(data);
+			dp.get(id, function (err, data) {
+				console.log(data);
+			})
+		})
+		dp.update(id, {"A2": {"before": "7", now: "34"}}, function (err, data) {
+			console.log(data);
+			dp.get(id, function (err, data) {
+				console.log(data);
+			})
+		})
+	})
 });
+
